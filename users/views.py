7d92dc1b7214261122ff2856704ee91b7a7a79df
Mohammadreza Pourhoseini <mohammadreza.pour.computer@gmail.com>
2022-11-11 23:12:00 +0330

    Complete the user profile page

--- users/views.py
+++ users/views.py
@@ -11,5 +11,12 @@ def profiles(request):
 
 def profile(request, username):
     profile_obj = Profile.objects.get(username=username)
-    context = {"profile": profile_obj}
+    top_skills = profile_obj.skill_set.exclude(description__exact="")
+    other_skills = profile_obj.skill_set.filter(description="")
+
+    context = {
+        "profile": profile_obj,
+        "topSkills": top_skills,
+        "otherSkills": other_skills
+    }
     return render(request, 'users/profile.html', context)
